421f5c3f88079ed67bc9c65758e9cefbd60eed46
oyu <12345yuu6789@gmail.com>
2024-10-04 14:25:13 +0900

    at368のB問題を解いた

---
+++ at368/DecreaseBad/answer.py
@@ -0,0 +1,33 @@
+# 自分で解けなかった
+# N = int(input())
+# A = list(map(int, input().split()))
+# judge = [True for _ in range(N)]
+# count = 0
+
+# while True:
+#   new_list = sorted(A)
+#   new_list[0] -= 1
+#   new_list[1] -= 1
+#   count += 1
+#   false_count = 0
+#   A = new_list
+#   for i in range(N):
+#     if A[i] <= 0:
+#       judge[i] = False
+#       false_count += 1
+      
+#   if false_count >= N-1:
+#     break
+
+print(count)
+
+N = int(input())
+A = list(map(int, input().split()))
+count = 0
+
+while A.count(0) < N-1:
+  A.sort(reverse=True)
+  A[0] -= 1
+  A[1] -= 1
+  count += 1
+print(count)
