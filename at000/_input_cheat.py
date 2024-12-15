096f7741ddf5f00d58219d24bda88e31924a6d54
oyu <12345yuu6789@gmail.com>
2024-12-16 04:00:39 +0900

    といた

--- at000/_input_cheat.py
+++ at000/_input_cheat.py
@@ -19,6 +19,6 @@ a = [int(input()) for _ in range(N)]
 # 1 2 3 4
 # 5 6 7 8
 # 9 10 11 12
-a = [ list(map(int,input().split(" "))) for _ in range(N)]
+a = [list(map(int,input().split(" "))) for _ in range(N)]
 # print(a) -> [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]]
 
